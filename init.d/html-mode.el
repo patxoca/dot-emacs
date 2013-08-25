eead7bec7b66df04b7a0078778130ed40ff6b7a3
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2013-08-25 19:51:48 +0000

    S'inclou "zencoding-mode". S'activa i configura en "html-mode". Es customiza en al instància superjul.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@290 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/html-mode.el
+++ init.d/html-mode.el
@@ -11,10 +11,16 @@
 ;;; Code:
 
 (require 'rainbow-mode)
+(require 'zencoding-mode)
+
+
+(eval-after-load "zencoding-mode"
+  '(progn
+     (define-key zencoding-mode-keymap '[(control ?j)] nil)))
 
 (add-hook 'html-mode-hook
           #'(lambda ()
               (rainbow-mode t)
-              ))
+              (zencoding-mode t)))
 
 ;;; html-mode.el ends here
