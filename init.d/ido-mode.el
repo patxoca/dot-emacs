636823c4cce4b791c77cf0363c9bad8fa741b8f6
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2013-12-28 13:29:13 +0000

    Vaig a donar-li una oportunitat a 'ido'. S'afegeix un mòdul de configuració i s'activa per la instància 'superjul'.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@311 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/ido-mode.el
@@ -0,0 +1,20 @@
+;;; ido-mode.el --- inicialitzacio de ido-mode
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; Code:
+
+(require 'ido)
+
+(ido-mode 1)
+(customize-set-variable 'ido-enable-flex-matching t)
+;;; (customize-set-variable 'ido-everywhere t)
+(customize-set-variable 'ido-ignore-directories
+ (quote ("\\`CVS/" "\\`\\.\\./" "\\`\\./" "\\`\\.svn/" "\\`\\.ropeproject/")))
+
+;;; 'ido-mode.el ends here
