8e1830983e0233c7f5025d7fbd2a97ec9de6ad94
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2013-12-30 18:59:50 +0000

    S'insereix 'yas-ido-prompt' com primera opció per triar entre múltiples snippets.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@317 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/yasnippet.el
+++ init.d/yasnippet.el
@@ -15,6 +15,8 @@
 (setq yas-snippet-dirs
       (list (concat emacs-startup-dir "/shared/yasnippets/local")
             (concat emacs-startup-dir "/shared/yasnippets/snippets")))
+(setq yas-prompt-functions '(yas-ido-prompt yas-x-prompt yas-completing-prompt))
+
 (yas-global-mode 1)
 
 ;;; yasnippet.el ends here
