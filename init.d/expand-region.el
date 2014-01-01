d3047965f1d23d7e773615441f678252b3130fd9
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-01-01 18:57:17 +0000

    S'inclou 'expand-region' i s'activa en 'superjul'.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@334 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/expand-region.el
@@ -0,0 +1,20 @@
+;;; expand-region.el --- inicialitza expand-region
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(require 'expand-region)
+
+
+
+;;; 'expand-region.el ends here
