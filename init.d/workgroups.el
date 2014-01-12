ebd61118cfcbbd358f96223dd07fcb6fa8625171
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-01-12 21:25:18 +0000

    S'afegeix 'workgroup.el'. Existeix a elpa/marmalade però és una versio lleugerament més vella.
    S'afegeix un modul per inicializar-lo i s'activa en les instàncies
    'superjul' i 'ritic23'.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@348 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/workgroups.el
@@ -0,0 +1,21 @@
+;;; workgroups.el --- inicalització de workgroups
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(require 'workgroups)
+
+(setq wg-prefix-key (kbd "C-c w"))
+(workgroups-mode 1)
+
+;;; workgroups.el ends here
