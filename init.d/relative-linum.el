f5bcba5df3e0a25d95b226df60c27ee6ff7cbf48
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-07-16 16:45:02 +0000

    S'afegeix el paquet 'relative-linum' i s'activa per defecte, en la instancia 'superjul', per tots els modes derivats de 'prog.mode'.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@358 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/relative-linum.el
@@ -0,0 +1,21 @@
+;;; relative-linum.el --- inicialització de 'relative-linum'
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(require 'relative-linum)
+
+(add-hook 'prog-mode-hook (lambda () (linum-mode 1)))
+
+
+;;; relative-linum.el ends here
