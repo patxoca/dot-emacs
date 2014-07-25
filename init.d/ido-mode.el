6c32d2b65b9a0ebf105bb7087f213db3dd21f1e1
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-07-25 19:42:14 +0000

    S'afegeix l'activació de 'ido-vertical-mode', si està disponible, al mòdul d'inicialització de 'ido-mode'.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@360 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/ido-mode.el
+++ init.d/ido-mode.el
@@ -12,6 +12,10 @@
 (require 'ido)
 
 (ido-mode 1)
+
+(when (require 'ido-vertical-mode nil 'noerror)
+  (ido-vertical-mode 1))
+
 (customize-set-variable 'ido-enable-flex-matching t)
 ;;; (customize-set-variable 'ido-everywhere t)
 (customize-set-variable 'ido-ignore-directories
