725392e8cef9dc5a443fcf2d4703be5ad3922067
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-07-27 19:35:48 +0000

    Al eliminar el mòdul 'yasnippet' del repositori canvia la ruta dels snippets predefinits (subministrats pel paquet).
    La variable 'yas-snippet-dirs' inclou la ruta corresponent al
    directori on el gestor de paquets ha instal·lat els snippets. Enlloc
    de sobreescriure el valor de 'yas-snippet-dirs' se li afegeix el
    directori dels snippets locals.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@370 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/yasnippet.el
+++ init.d/yasnippet.el
@@ -8,15 +8,13 @@
 ;;
 
 ;;; Code:
+
 (require 'yasnippet)
-; carrega funcions d'utilitat
 (require 'arv-py)
 
-(setq yas-snippet-dirs
-      (list (concat emacs-startup-dir "/shared/yasnippets/local")
-            (concat emacs-startup-dir "/shared/yasnippets/snippets")))
+(add-to-list 'yas-snippet-dirs
+             (arv/startup-get-absolute-path "shared/yasnippets/local"))
 (setq yas-prompt-functions '(yas-ido-prompt yas-x-prompt yas-completing-prompt))
-
 (yas-global-mode 1)
 
 ;;; yasnippet.el ends here
