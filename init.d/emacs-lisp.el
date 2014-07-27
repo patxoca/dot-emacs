a8b3e4370b26a984503cd0b407bd5b0abdae0e69
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-07-27 20:29:00 +0000

    El keybinding C-<right> i C-<left> de paredit és realment molest, s'intercanvia amb M-<right> i M-<left> i es restaura el binding original.
    Aprofitant l'avinentesa es factoritza la redefinició d'un parell de
    tecles per 'emacs-lisp-mode' fora del hook. Açò evita que es
    redefinixin per cada buffer obert (no suposa cap problema però és
    feo).
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@372 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/emacs-lisp.el
+++ init.d/emacs-lisp.el
@@ -22,18 +22,26 @@
 (require 'paredit)
 (require 'pretty-lambdada)
 
+(eval-after-load "paredit"
+  '(progn
+     (define-key paredit-mode-map (kbd "C-<right>") 'right-word)
+     (define-key paredit-mode-map (kbd "C-<left>") 'left-word)
+     (define-key paredit-mode-map (kbd "M-<right>") 'paredit-forward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "M-<left>") 'paredit-forward-barf-sexp)))
+
+(eval-after-load "lisp-mode"
+  '(progn
+     (local-set-key (kbd "s-SPC") 'company-complete)
+     (local-set-key '[f9] (lambda () (interactive) (ert t)))))
+
 (add-hook 'emacs-lisp-mode-hook
           #'(lambda ()
               ;; company-mode (complete anything)
               (company-mode t)
               (eldoc-mode t)
-              (local-set-key (kbd "s-SPC") 'company-complete)
               (paredit-mode 1)
               (elisp-slime-nav-mode 1)
-              (when (require 'pretty-lambdada nil 'noerror)
-                (pretty-lambda-mode t))
-              (local-set-key '[f9] (lambda ()
-                                     (interactive)
-                                     (ert t)))))
+              (pretty-lambda-mode t)
+              ))
 
 ;;; 'emacs-lisp.el ends here
