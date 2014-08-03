d3f3d817bfaac555f18db4053df49ec499807502
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-08-03 19:01:53 +0000

    Es crea una instància amb configuració mínima per aconsseguir la major velocitat d'arranc. Es pot activar definint la variable d'entorn 'EMACS_INSTANCE'.
    Aquesta instància activa un tema, oculta la barra de menú etc. i
    defineix la mida de la lletra.
    
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@393 a9598837-e506-0410-a967-fbf0066ee963

---
+++ instances/fast/customize.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 107 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
