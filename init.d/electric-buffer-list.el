be9edf07be5ecac7ebd6183ecfc4f11a4786a4e3
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-08-04 12:01:37 +0000

    Es defineix una funció que genera 'faces' sota demanda, com que no cap enlloc es crea el mòdul 'assorted.el' per aquesta mena de funcions.
    Es modifica la inicialització de 'electric-buffer-list' per generar 15
    faces i assignarles a diferents tipus de buffers.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@395 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/electric-buffer-list.el
+++ init.d/electric-buffer-list.el
@@ -11,15 +11,20 @@
 
 ;;; Code:
 
-(setq buffer-menu-buffer-font-lock-keywords
-      '(("^....[*]Man .*Man.*"   . font-lock-variable-name-face) ;Man page
-        (".*Dired.*"             . font-lock-comment-face)       ; Dired
-        ("^....[*]shell.*"       . font-lock-preprocessor-face)  ; shell buff
-        (".*[*]scratch[*].*"     . font-lock-function-name-face) ; scratch buffer
-        ("^.*[*]svn-.*"          . font-lock-builtin-face)       ; svn buffers
-        ("^....[*].*"            . font-lock-string-face)        ; "*" named buffers
-        ("^..[*].*"              . font-lock-constant-face)      ; Modified
-        ("^.[%].*"               . font-lock-keyword-face)))     ; Read only
+(eval-after-load "ebuff-menu"
+  '(progn
+     (arv/generate-lab-faces "electric-buffer" 15)
+     (setq buffer-menu-buffer-font-lock-keywords
+           '(
+             (".*Dired.*"             . 'electric-buffer-01-face) ; Dired
+             ("^....[*]shell.*"       . 'electric-buffer-02-face) ; shell buff
+             (".*[*]scratch[*].*"     . 'electric-buffer-03-face) ; scratch buffer
+             ("^.*[*]svn-.*"          . 'electric-buffer-04-face) ; svn buffers
+             ("^....[*].*"            . 'electric-buffer-05-face) ; "*" named buffers
+             ("^..[*].*"              . 'electric-buffer-06-face) ; Modified
+             ("^.[%].*"               . 'electric-buffer-07-face) ; Read only
+             ("^....[*]Man .*Man.*"   . 'electric-buffer-08-face) ; Man page
+             ))))
 
 (defun buffer-menu-custom-font-lock  ()
   (let ((font-lock-unfontify-region-function
