271c0b829dc92b91283e63d20d6df039fb31cbcc
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-08-05 17:29:32 +0000

    Added the package 'google-this'.
    https://github.com/Bruce-Connor/emacs-google-this
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@408 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/google-this.el
@@ -0,0 +1,19 @@
+;;; google-this.el --- setup google-this
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(setq google-this-keybind (kbd "C-c g"))
+(google-this-mode 1)
+
+;;; google-this.el ends here
