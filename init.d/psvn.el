a211d498a7f1899e700e92751dc0f0450e70f1a3
alex <alex@a9598837-e506-0410-a967-fbf0066ee963>
2014-08-17 17:00:29 +0000

    Added some 'autoload' definitions related to 'psvn.el'. Enable the module on some instances.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@424 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/psvn.el
@@ -0,0 +1,20 @@
+;;; psvn.el --- psvn customization
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(autoload 'svn-file-show-svn-diff "psvn" "" t nil)
+(autoload 'svn-file-revert "psvn" "" t nil)
+(autoload 'svn-status-show-svn-log "psvn" "" t nil)
+
+;;; psvn.el ends here
