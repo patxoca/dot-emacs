98da970af79435d6827bc217f162ca9baf4bc18d
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-08-18 09:49:18 +0000

    Added an init module in order to store assorted customizations that don't fit anywhere else.
    Added a customization to delete trailing whitespace on save.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@426 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/assorted.el
@@ -0,0 +1,20 @@
+;;; assorted.el --- assorted customization
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+
+;;; History:
+;;
+
+
+;;; Code:
+
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+
+;;; assorted.el ends here
