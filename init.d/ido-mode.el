64cb4838731f6a3fbd06af8244cd166f4a2254a3
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-08-21 10:33:23 +0000

    Defined new motion keybindings for ido in vertical mode.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@450 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/ido-mode.el
+++ init.d/ido-mode.el
@@ -9,11 +9,20 @@
 
 ;;; Code:
 
+(require 'ido)
+
+(defun arv/ido-vertical-define-keys ()
+  ;; more intuitive keybinds for vertical-mode
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+  (define-key ido-completion-map (kbd "<up>") 'ido-prev-match))
 
 (ido-mode 1)
 
 (when (require 'ido-vertical-mode nil 'noerror)
-  (ido-vertical-mode 1))
+  (ido-vertical-mode 1)
+  (add-hook 'ido-setup-hook 'arv/ido-vertical-define-keys))
 
 (customize-set-variable 'ido-enable-flex-matching t)
 ;;; (customize-set-variable 'ido-everywhere t)
