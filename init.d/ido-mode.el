6551eee54340c2406020068538e04e96083dda58
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-08-21 17:02:24 +0000

    Added a couple of customizations to ido-mode:
    * added two keybindings to navigate the work directories in vertical
      mode
    
    * enable ide everywhere
    
    * Inhibit switching to other directory when no matches are found in
      the current directory.
    
    * reduce the max size of work directory list. I expect that this way
      work directory navigation will be more usable.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@453 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/ido-mode.el
+++ init.d/ido-mode.el
@@ -14,17 +14,26 @@
   (define-key ido-completion-map (kbd "<down>") 'ido-next-match)
   (define-key ido-completion-map (kbd "<up>") 'ido-prev-match)
   (define-key ido-completion-map (kbd "<left>") 'ido-prev-match-dir)
-  (define-key ido-completion-map (kbd "<right>") 'ido-next-match-dir))
+  (define-key ido-completion-map (kbd "<right>") 'ido-next-match-dir)
+  (define-key ido-completion-map (kbd "M-<left>") 'ido-prev-work-directory)
+  (define-key ido-completion-map (kbd "M-<right>") 'ido-next-work-directory)
+  )
+
+
 
 (ido-mode 1)
+(ido-everywhere 1)
 
 (when (require 'ido-vertical-mode nil 'noerror)
   (ido-vertical-mode 1)
   (add-hook 'ido-setup-hook 'arv/ido-vertical-define-keys))
 
+;; Inhibit switching to other directory when no matches are found in
+;; the current directory.
+(customize-set-variable 'ido-auto-merge-work-directories-length -1)
 (customize-set-variable 'ido-enable-flex-matching t)
-;;; (customize-set-variable 'ido-everywhere t)
 (customize-set-variable 'ido-ignore-directories
  (quote ("\\`CVS/" "\\`\\.\\./" "\\`\\./" "\\`\\.svn/" "\\`\\.ropeproject/")))
+(customize-set-variable 'ido-max-work-directory-list 10)
 
 ;;; 'ido-mode.el ends here
