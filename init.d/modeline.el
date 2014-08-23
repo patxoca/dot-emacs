c5642a1f1e1f9e5daa3a9164e9828298ad64c30f
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-08-23 17:31:32 +0000

    Added a module to customize the modeline in order to make some room.
    * diminish minor modes
    * remove unwanted information
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@456 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/modeline.el
@@ -0,0 +1,65 @@
+;;; modeline.el --- mode line customization
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+;;; Local keybindings:
+;;
+;; C-M-q: does some amazing stuff
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(require 'diminish)
+(require 's)
+
+
+;; List of modes to be diminished. Each item has the format:
+;;
+;; (MODE PACKAGE REPLACEMENT)
+;;
+;; - MODE is required
+;; - PACKAGE defaults to MODE without the '-mode' suffix.
+;; - REPLACEMENT defaults to ""
+(setq arv/modeline--diminished-modes
+      '((autopair-mode)
+        (company-mode)
+        (eldoc-mode)
+        (elisp-slime-nav-mode)
+        (google-this-mode)
+        (hi-lock-mode)
+        (outline-minor-mode "outline")
+        (paredit-mode)
+        (ropemacs-mode "python-mode")
+        (workgroups-mode)
+        (yas-minor-mode "yasnippet")))
+
+(defun arv/modeline--diminish-modes ()
+  (dolist (item arv/modeline--diminished-modes)
+    (let ((mode (car item))
+          (package (or (cadr item)
+                       (s-chop-suffix "-mode" (symbol-name (car item)))))
+          (replacement (or (caddr item) "")))
+      ;; diminishing must be delayed until the corresponding package
+      ;; is loaded and the minor mode, hopefully, gets defined.
+      (eval-after-load package
+        (list 'diminish (list 'quote mode) replacement)))))
+
+
+;; hide line and column number
+(column-number-mode 0)
+(line-number-mode 0)
+
+;; hide time and system load
+(display-time-mode 0)
+
+;; diminish selected modes
+(arv/modeline--diminish-modes)
+
+;;; modeline.el ends here
