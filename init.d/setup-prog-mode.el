7a5c44d773d9841becd2c224e720bea8de169eb1
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-12-29 10:48:24 +0000

    Highlight some labels in programming modes.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@560 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/setup-prog-mode.el
+++ init.d/setup-prog-mode.el
@@ -20,6 +20,17 @@
             (linum-mode 0)
             (show-paren-mode 1)
             (set-variable 'show-trailing-whitespace t)
+            ;; hi-lock
+            (hi-lock-mode 1)
+            (hi-lock-face-buffer "\\<FIXME" 'hi-red-b)
+            (hi-lock-face-buffer "\\<HACK" 'hi-red-b)
+            (hi-lock-face-buffer "\\<OPTIMIZE" 'hi-red-b)
+            (hi-lock-face-buffer "\\<REVIEW" 'hi-red-b)
+            (hi-lock-face-buffer "\\<TODO" 'hi-red-b)
+
+            (hi-lock-face-buffer "\\<IMPORTANT" 'hi-green)
+            (hi-lock-face-buffer "\\<NOTE" 'hi-green)
+            (hi-lock-face-buffer "\\<WARNING" 'hi-green)
             ))
 
 ;;; prog-mode.el ends here
