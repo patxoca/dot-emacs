25c6b7c576e1bd16f8976782ba2ce9a2473edf22
Alexis Roda <alexis.roda.villalonga@gmail.com>
2015-03-22 19:10:01 +0000

    Added config for engine-mode.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@621 a9598837-e506-0410-a967-fbf0066ee963

---
+++ init.d/setup-engine-mode.el
@@ -0,0 +1,55 @@
+;;; setup-engine-mode.el --- customization for engine-mode
+
+;; $Id$
+
+
+;;; Commentary:
+;;
+
+;;; Local keybindings:
+;;
+;; C-M-q: does some amazing stuff
+
+;;; History:
+;;
+
+
+;;; Code:
+
+(require 'engine-mode)
+(engine-mode t)
+
+(defengine django
+  "https://docs.djangoproject.com/search/?q=%s&release=11"
+  "d")
+
+(defengine dojo
+  "https://www.google.es/search?q=%s+site:dojotoolkit.org"
+  "D")
+
+(defengine emacs-stack-exchange
+  "http://emacs.stackexchange.com/search?q=%s"
+  "e")
+
+(defengine google
+  "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
+  "g")
+
+(defengine github
+  "https://github.com/search?ref=simplesearch&q=%s"
+  "G")
+
+(defengine rfcs
+  "http://pretty-rfc.herokuapp.com/search?q=%s"
+  "r")
+
+(defengine stack-overflow
+  "https://stackoverflow.com/search?q=%s"
+  "s")
+
+(defengine wikipedia
+  "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+  "w")
+
+
+;;; engine-mode.el ends here
