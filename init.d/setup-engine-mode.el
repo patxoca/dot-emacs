fb627e41750af1e888b0b8a186d275332c585889
Alexis Roda <alexis.roda.villalonga@gmail.com>
2015-05-22 15:51:30 +0000

    Update engine-mode setup.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@652 a9598837-e506-0410-a967-fbf0066ee963

--- init.d/setup-engine-mode.el
+++ init.d/setup-engine-mode.el
@@ -21,42 +21,43 @@
 
 (defengine diec2
   "dlc.iec.cat/results.asp?txtEntrada=%s"
-  "c")
+  :keybinding "c"
+  :term-transformation-hook (lambda (term) (encode-coding-string term latin-1)))
 
 (defengine django
   "https://docs.djangoproject.com/search/?q=%s&release=11"
-  "d")
+  :keybinding "d")
 
 (defengine dojo
   "https://www.google.es/search?q=%s+site:dojotoolkit.org"
-  "D")
+  :keybinding "D")
 
 (defengine emacs-stack-exchange
   "http://emacs.stackexchange.com/search?q=%s"
-  "e")
+  :keybinding "e")
 
 (defengine google
   "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
-  "g")
+  :keybinding "g")
 
 (defengine github
   "https://github.com/search?ref=simplesearch&q=%s"
-  "G")
+  :keybinding "G")
 
 (defengine rfcs
   "http://pretty-rfc.herokuapp.com/search?q=%s"
-  "r")
+  :keybinding "r")
 
 (defengine stack-overflow
   "https://stackoverflow.com/search?q=%s"
-  "s")
+  :keybinding "s")
 
 (defengine wikipedia
   "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
-  "w")
+  :keybinding "w")
 
 (defengine wordreference
   "http://www.wordreference.com/redirect/translation.aspx?w=%s&dict=enes"
-  "W")
+  :keybinding "W")
 
 ;;; engine-mode.el ends here
