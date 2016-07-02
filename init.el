243e8420e2e62f4378e064ec3651f1cdd6ddf8bc
Alexis Roda <alexis.roda.villalonga@gmail.com>
2016-07-02 09:47:03 +0000

    Added 'elpy' repository to 'package-archives'.
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@704 a9598837-e506-0410-a967-fbf0066ee963

--- init.el
+++ init.el
@@ -131,7 +131,8 @@ directoris que contenen codi lisp sota el control de l'usuari."
        (expand-file-name "~/.emacs.d/elpa/package.el")))
   (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
                            ("gnu" . "http://elpa.gnu.org/packages/")
-                           ("melpa stable" . "http://melpa-stable.milkbox.net/packages/")))
+                           ("melpa stable" . "http://melpa-stable.milkbox.net/packages/")
+                           ("elpy" . "http://jorgenschaefer.github.io/packages/")))
   (package-initialize))
 
 (defun arv/startup-load-autoloads ()
