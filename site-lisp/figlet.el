38511922b51f7b1b51f206d8dcc5b42451501166
Alexis Roda <alexis.roda.villalonga@gmail.com>
2018-04-01 23:08:08 +0000

    Canvis varis a "figlet".
    - afegir funciones "figlet-no-comment" i "banner-no-comment".
    
    - afegir cookies autoload
    
    - eliminar trailing whitespace
    
    - renombrar funcions per adaptar-les a les recomanacions: totes
      comencen pel prefix "figlet-" i les funcions privades utilitzen
      doble guió.
    
    - adaptar el codi a les recomanacions de checkdoc.
    
    
    git-svn-id: svn://localhost/emacslib/emacslib/trunk@817 a9598837-e506-0410-a967-fbf0066ee963

--- site-lisp/figlet.el
+++ site-lisp/figlet.el
@@ -1,9 +1,14 @@
-;;;; Copyright (c) 2007 Martin Giese. All rights reserved. 
+;;; figlet.el --- FIGlet definitions for Emacs.
+;;;; Copyright (c) 2007 Martin Giese. All rights reserved.
 ;;;;
 ;;;; Author: Martin Giese
 ;;;; Author: Dimitry Gashinsky
-;;;; Id: 295eb73b-9e95-46a6-9cce-366436f9712f 
+;;;; Id: 295eb73b-9e95-46a6-9cce-366436f9712f
 ;;;;
+
+;;; Commentary:
+;;
+
 ;;;; FIGlet definitions for Emacs.
 ;;;;
 ;;;; Use this to separate sections in TeX files, Program source, etc.
@@ -15,16 +20,17 @@
 ;;;; C-u M-x figlet  to be asked for the font first.
 ;;;; M-x banner      for an old-fashioned banner font.
 ;;;;
-;;;; These functions use comment-region to wrap the figlet output 
+;;;; These functions use comment-region to wrap the figlet output
 ;;;; in comments.
 ;;;;
 
-;;;  _____ ___ ____ _      _     ____  _          __  __ 
+;;;  _____ ___ ____ _      _     ____  _          __  __
 ;;; |  ___|_ _/ ___| | ___| |_  / ___|| |_ _   _ / _|/ _|
-;;; | |_   | | |  _| |/ _ \ __| \___ \| __| | | | |_| |_ 
+;;; | |_   | | |  _| |/ _ \ __| \___ \| __| | | | |_| |_
 ;;; |  _|  | | |_| | |  __/ |_   ___) | |_| |_| |  _|  _|
-;;; |_|   |___\____|_|\___|\__| |____/ \__|\__,_|_| |_|  
-                                                     
+;;; |_|   |___\____|_|\___|\__| |____/ \__|\__,_|_| |_|
+
+;;; Code:
 
 (defgroup figlet nil
   "Display large characters made up of ordinary screen characters."
@@ -33,9 +39,9 @@
   :prefix "figlet-")
 
 (defcustom figlet-font-dir nil
-  "Change the default font directory. FIGlet looks for fonts
-   first in the default directory and then in the current
-   directory."
+  "Change the default font directory.
+FIGlet looks for fonts first in the default directory and then in
+the current directory."
   :tag "FIGlet Font Directory"
   :group 'figlet
   :type '(choice (const nil) string))
@@ -49,49 +55,73 @@
 (defconst figlet-font-file-regexp "\\.flf$")
 (defconst figlet-match-font-name-regexp "^\\([^.]*\\)\\.flf$")
 
-(defun figlet-font-name-for-file (filename)
+(defun figlet--font-name-for-file (filename)
   (string-match figlet-match-font-name-regexp filename)
   (match-string 1 filename))
 
-(defun figlet-font-names ()
-  (mapcar 'figlet-font-name-for-file
+(defun figlet--font-names ()
+  (mapcar 'figlet--font-name-for-file
           (directory-files figlet-font-dir nil figlet-font-file-regexp)))
 
-(defun read-figlet-font (prompt)
-  (let* ((figlet-fonts (figlet-font-names))
+(defun figlet--read-font (prompt)
+  (let* ((figlet-fonts (figlet--font-names))
          (font-alist (mapcar (lambda (x) (list x)) figlet-fonts)))
     (completing-read prompt font-alist)))
 
-(defun call-figlet (font string)
+(defun figlet--call (font string)
   (push-mark)
   (apply #'call-process
          `("figlet" nil ,(current-buffer) nil
-                    ,@(if (null figlet-font-dir) `()
-                          `("-d" ,(expand-file-name figlet-font-dir)))
-                    ,@(if (null font) `()
-                          `("-f" ,font))
-                    ,string))
+           ,@(if (null figlet-font-dir) `()
+               `("-d" ,(expand-file-name figlet-font-dir)))
+           ,@(if (null font) `()
+               `("-f" ,font))
+           ,string))
   (exchange-point-and-mark))
 
-(defun figlet-block-comment-region ()
+(defun figlet--comment-region ()
   (comment-region (region-beginning) (region-end)
                   (if (member major-mode '(emacs-lisp-mode lisp-mode scheme-mode))
                       3
-                      nil)))
+                    nil)))
 
+(defun figlet--interactive (cpa)
+  (if cpa
+      (let ((font (figlet--read-font "FIGlet Font: "))
+            (text (read-string "FIGlet Text: ")))
+        (list text font))
+    (list (read-string "FIGlet Text: ") nil)))
+
+
+;;;###autoload
 (defun figlet (s &optional font)
-  (interactive 
-   (if current-prefix-arg
-       (let ((font (read-figlet-font "FIGlet Font: "))
-             (text (read-string "FIGlet Text: ")))
-         (list text font))
-       (list (read-string "FIGlet Text: ") nil)))
+  "Insert string S as a FIGlet as a comment.
+Optional argument FONT font, nil for default."
+  (interactive (figlet--interactive current-prefix-arg))
   (save-excursion
-    (call-figlet (if (null font) figlet-default-font font) s)
-    (figlet-block-comment-region)))
+    (figlet--call (if (null font) figlet-default-font font) s)
+    (figlet--comment-region)))
 
-(defun banner (s) 
+;;;###autoload
+(defun figlet-no-comment (s &optional font)
+  "Insert string S as a FIGlet without comment.
+Optional argument FONT font, nil for default."
+  (interactive  (figlet--interactive current-prefix-arg))
+  (save-excursion
+    (figlet--call (if (null font) figlet-default-font font) s)))
+
+;;;###autoload
+(defun banner (s)
+  "Insert string S as a FIGlet comment using the banner font."
   (interactive "sFIGlet Banner Text: ")
   (figlet s "banner"))
 
+;;;###autoload
+(defun banner-no-comment (s)
+  "Insert string S as a no comment FIGlet using the banner font."
+  (interactive "sFIGlet Banner Text: ")
+  (figlet-no-comment s "banner"))
+
 (provide 'figlet)
+
+;;; figlet.el ends here
